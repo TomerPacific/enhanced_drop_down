58531c7691872f3f7189166d34f6da00a855f3bb
unknown <tomerpacific@gmail.com>
2021-03-19 21:11:22 +0200

    EnhancedDropdown refactored entire widget by exposing two constructors, correcting behavior, adding comments, renaming methods

--- lib/enhanced_drop_down.dart
+++ lib/enhanced_drop_down.dart
@@ -7,14 +7,19 @@ import 'dart:convert' as convert;
 class EnhancedDropDown extends StatefulWidget {
   final ValueChanged<String> valueReturned;
 
-  EnhancedDropDown(
-      {required Key key,
-      this.dropdownLabelTitle: "",
-      required this.dataSource,
-      this.defaultOptionText: "",
-      this.urlToFetchData: "",
-      required this.valueReturned})
-      : super(key: key);
+  ///Constructor that accepts a list of elements to be the data source for the dropdown
+  EnhancedDropDown.withData(
+      {required this.dropdownLabelTitle,
+        required this.dataSource,
+        required this.defaultOptionText,
+        required this.valueReturned}) : urlToFetchData = null;
+
+  ///Constructor that accepts an endpoint in URI form to fetch the data from
+  EnhancedDropDown.withEndpoint({
+    required this.dropdownLabelTitle,
+    required this.defaultOptionText,
+    required this.urlToFetchData,
+    required this.valueReturned}) : dataSource = null;
 
   /// Holds the default text to show when nothing is selected in the dropdown
   final String defaultOptionText;
@@ -23,10 +28,10 @@ class EnhancedDropDown extends StatefulWidget {
   final String dropdownLabelTitle;
 
   /// The endpoint to fetch the data that gets used by the dropdown
-  final String urlToFetchData;
+  final Uri? urlToFetchData;
 
-  /// A list which holds the data received from the endpoint
-  final List<String> dataSource;
+  /// A list which holds the data if we don't want to make a network request
+  final List<String>? dataSource;
 
   @override
   _EnhancedDropDownState createState() => _EnhancedDropDownState();
@@ -34,23 +39,28 @@ class EnhancedDropDown extends StatefulWidget {
 
 class _EnhancedDropDownState extends State<EnhancedDropDown> {
   List<DropdownMenuItem<String>> _data = [];
-  String _selected = "";
+  String _selected = "Chosen Value";
 
   @override
   void initState() {
     super.initState();
-    _loadData();
+    _loadDataForDropdown();
   }
 
-  /// Fetches the data that the dropdown uses from the endpoint
-  void _loadData() async {
-    _data = [];
-    if (widget.urlToFetchData.isNotEmpty) {
-      var url = Uri.https(widget.urlToFetchData, "");
-      var response = await http.get(url);
+  /// Responsible for loading the data that the dropdown uses
+  void _loadDataForDropdown() async {
+    _data = const [];
+
+    List<DropdownMenuItem<String>> menuItems = [];
+    menuItems.add(new DropdownMenuItem(
+      child: new Text(_selected),
+      value: _selected,
+    ));
+
+    if (widget.urlToFetchData != null) {
+      var response = await http.get(widget.urlToFetchData!);
       if (response.statusCode == 200) {
         Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
-        List<DropdownMenuItem<String>> menuItems = [];
         jsonResponse.forEach((key, value) {
           menuItems.add(new DropdownMenuItem(
             child: new Text(value.toString()),
@@ -61,15 +71,18 @@ class _EnhancedDropDownState extends State<EnhancedDropDown> {
           _data = menuItems;
         });
       } else {
-        print("Request failed with status: ${response.statusCode}.");
+        print("EnhancedDropDownWidget Request failed with status: ${response.statusCode}.");
       }
-    } else {
-      for (int i = 0; i < widget.dataSource.length; i++) {
-        _data.add(new DropdownMenuItem(
-          child: new Text(widget.dataSource[i]),
-          value: widget.dataSource[i],
+    } else if (widget.dataSource != null) {
+      for (int i = 0; i < widget.dataSource!.length; i++) {
+        menuItems.add(new DropdownMenuItem(
+          child: new Text(widget.dataSource![i]),
+          value: widget.dataSource![i],
         ));
       }
+      setState(() {
+        _data = menuItems;
+      });
     }
   }
 
@@ -78,19 +91,20 @@ class _EnhancedDropDownState extends State<EnhancedDropDown> {
     if (_data.length == 0) {
       return new Container();
     } else {
-      return new Scaffold(
-          body: new Column(
+      return new Container(
+        height: 100,
+          child: new Column(
         children: <Widget>[
           new Text(widget.dropdownLabelTitle, textDirection: TextDirection.ltr),
           DropdownButton(
-              value: _selected,
-              items: _data,
-              hint: new Text(widget.defaultOptionText),
-              onChanged: (value) {
-                _selected = value as String;
-                widget.valueReturned(_selected);
-                setState(() {});
-              })
+                  value: _selected,
+                  items: _data,
+                  hint: new Text(widget.defaultOptionText),
+                  onChanged: (value) {
+                    _selected = value as String;
+                    widget.valueReturned(_selected);
+                    setState(() {});
+                  })
         ],
       ));
     }
