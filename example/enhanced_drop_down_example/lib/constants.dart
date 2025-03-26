af638ebee533d1126c356496dcf819c83f627a7d
unknown <tomerpacific@gmail.com>
2025-03-26 23:07:26 +0200

    EDW creating constants file for example

---
+++ example/enhanced_drop_down_example/lib/constants.dart
@@ -0,0 +1,5 @@
+
+const String ENDPOINT = "raw.githubusercontent.com";
+const String PATH = "/TomerPacific/enhanced_drop_down/refs/heads/feature/refactor/example/enhanced_drop_down_example/";
+const String PERSON_PATH = PATH + "person.json";
+const String PERSON_LIST_PATH = PATH + "personList.json";
