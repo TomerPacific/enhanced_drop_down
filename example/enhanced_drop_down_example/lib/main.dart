b5d9c04a89b12144c6b0452fba866176aa2ca54a
unknown <tomerpacific@gmail.com>
2025-03-26 23:12:50 +0200

    EDW adding more constants

--- example/enhanced_drop_down_example/lib/main.dart
+++ example/enhanced_drop_down_example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Enhanced Dropdown Widget Example',
+      title: APP_TITLE,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -37,14 +37,14 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Enhanced Dropdown Widget Example'),
+          title: Text(APP_TITLE),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               EnhancedDropDown.withData(
-                  dropdownLabelTitle: "EDW With Data Object",
+                  dropdownLabelTitle: EDW_WITH_DATA_OBJECT_LABEL,
                   dataSource: [
                     Person("First", "Last", 10),
                     Person("Last", "First", 20)
@@ -53,31 +53,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   valueReturned: (chosenValue) {
                     print("EDW the chosen value is: $chosenValue");
                   },
-                  fieldToPresent: "firstName"),
+                  fieldToPresent: FIRST_NAME_FIELD_KEY),
               EnhancedDropDown.withData(
-                  dropdownLabelTitle: "EDW With Data (String)",
+                  dropdownLabelTitle: EDW_WITH_DATA_STRING_LABEL,
                   dataSource: ["A", "B"],
                   defaultOptionText: "Choose Letter",
                   valueReturned: (chosenValue) {
                     print("EDW the chosen value is: $chosenValue");
                   }),
               EnhancedDropDown.withEndpoint(
-                  dropdownLabelTitle: "EDW With Endpoint (One Object)",
+                  dropdownLabelTitle: EDW_WITH_ENDPOINT_OBJECT_LABEL,
                   defaultOptionText: "Choose a first name",
                   urlToFetchData: Uri.https(ENDPOINT, PERSON_PATH),
                   valueReturned: (chosenValue) {
                     print("EDW the chosen value is: $chosenValue");
                   },
-                  fieldToPresent: "firstName"),
+                  fieldToPresent: FIRST_NAME_FIELD_KEY),
               EnhancedDropDown.withEndpoint(
                   dropdownLabelTitle:
-                      "EDW With Endpoint Object (List of Objects)",
+                      EDW_WITH_ENDPOINT_OBJECT_LIST_LABEL,
                   defaultOptionText: "Choose a first name",
                   urlToFetchData: Uri.https(ENDPOINT, PERSON_LIST_PATH),
                   valueReturned: (chosenValue) {
                     print("EDW the first name of the person chosen is: $chosenValue");
                   },
-                  fieldToPresent: "firstName")
+                  fieldToPresent: FIRST_NAME_FIELD_KEY)
             ],
           ),
         ));
