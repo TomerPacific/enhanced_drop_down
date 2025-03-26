aec308b9032fc65573f05944afa061167c0efc7f
unknown <tomerpacific@gmail.com>
2025-03-26 23:15:18 +0200

    EDW formatting file

--- example/enhanced_drop_down_example/lib/constants.dart
+++ example/enhanced_drop_down_example/lib/constants.dart
@@ -1,16 +1,14 @@
-
-
 const APP_TITLE = "Enhanced Dropdown Widget Example";
 const EDW_WITH_DATA_OBJECT_LABEL = "EDW With Data Object";
 const EDW_WITH_DATA_STRING_LABEL = "EDW With Data (String)";
 const EDW_WITH_ENDPOINT_OBJECT_LABEL = "EDW With Endpoint (One Object)";
-const EDW_WITH_ENDPOINT_OBJECT_LIST_LABEL = "EDW With Endpoint (List of Objects)";
-
+const EDW_WITH_ENDPOINT_OBJECT_LIST_LABEL =
+    "EDW With Endpoint (List of Objects)";
 const FIRST_NAME_FIELD_KEY = "firstName";
-
 
 //Network
 const String ENDPOINT = "raw.githubusercontent.com";
-const String PATH = "/TomerPacific/enhanced_drop_down/refs/heads/feature/refactor/example/enhanced_drop_down_example/";
+const String PATH =
+    "/TomerPacific/enhanced_drop_down/refs/heads/feature/refactor/example/enhanced_drop_down_example/";
 const String PERSON_PATH = PATH + "person.json";
 const String PERSON_LIST_PATH = PATH + "personList.json";
