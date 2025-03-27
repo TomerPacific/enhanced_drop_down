f52f9b3190c5a3c36e9d08451fe70bbab32c0e71
unknown <tomerpacific@gmail.com>
2025-03-27 19:00:00 +0200

    adding test for dropdown with array as data source

--- example/enhanced_drop_down_example/test/widget_test.dart
+++ example/enhanced_drop_down_example/test/widget_test.dart
@@ -13,4 +13,15 @@ void main() {
     expect(find.text('First'), findsOneWidget);
     expect(find.text('Last'), findsOneWidget);
   });
+
+  testWidgets('Pressing on dropdown with array as data source test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    expect(find.text(EDW_WITH_DATA_STRING_LABEL), findsOneWidget);
+
+    await tester.tap(find.text('Choose Letter'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('A'), findsOneWidget);
+    expect(find.text('B'), findsOneWidget);
+  });
 }
