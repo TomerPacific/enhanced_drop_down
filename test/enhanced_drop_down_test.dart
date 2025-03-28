59afc7993de5b52e5d0775420c359df473bb4302
unknown <tomerpacific@gmail.com>
2025-03-28 13:02:20 +0300

    EDW changing description of tests

--- test/enhanced_drop_down_test.dart
+++ test/enhanced_drop_down_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:enhanced_drop_down/enhanced_drop_down.dart';
 
 void main() {
-  testWidgets("Testing widget creation", (WidgetTester tester) async {
+  testWidgets("Testing finding label of dropdown", (WidgetTester tester) async {
     var widget = EnhancedDropDown.withData(
         defaultOptionText: "Choose Me",
         dropdownLabelTitle: "Label",
@@ -32,7 +32,7 @@ void main() {
     expect(titleFinder, findsOneWidget);
   });
 
-  testWidgets("Testing widget creation", (WidgetTester tester) async {
+  testWidgets("Testing finding default option text of dropdown", (WidgetTester tester) async {
     var widget = EnhancedDropDown.withData(
         defaultOptionText: "Choose Me",
         dropdownLabelTitle: "Label",
